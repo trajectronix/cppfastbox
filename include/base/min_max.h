673caebec7d9d110df12ce045cec7be4ea1b9870
小鸡快跑 <2283572185@qq.com>
2024-02-18 17:25:45 +0800

    最小化的min和max支持

---
+++ include/base/min_max.h
@@ -0,0 +1,48 @@
+/**
+ * @file platform.h
+ * @brief mix max函数实现
+ *
+ * @copyright Copyright 2024-present Trajectronix Open Source Group
+ *
+ */
+#pragma once
+#include <concepts>
+
+namespace cppfastbox
+{
+    namespace detail
+    {
+        template <typename type>
+        concept less_comparable = requires(const type& a, const type& b) {
+            { a < b } -> ::std::same_as<bool>;
+        };
+        template <typename type, typename func>
+        concept compare_function = requires(const type& a, const type& b, func f) {
+            { f(a, b) } -> ::std::same_as<bool>;
+        };
+    }  // namespace detail
+
+    template <::cppfastbox::detail::less_comparable type>
+    constexpr inline auto& min(const type& a, const type& b) noexcept
+    {
+        return a < b ? a : b;
+    }
+
+    template <typename type, ::cppfastbox::detail::compare_function<type> func>
+    constexpr inline auto& min(const type& a, const type& b, func f) noexcept
+    {
+        return f(a, b) ? a : b;
+    }
+
+    template <::cppfastbox::detail::less_comparable type>
+    constexpr inline auto& max(const type& a, const type& b) noexcept
+    {
+        return a < b ? b : a;
+    }
+
+    template <typename type, ::cppfastbox::detail::compare_function<type> func>
+    constexpr inline auto& max(const type& a, const type& b, func f) noexcept
+    {
+        return f(a, b) ? a : b;
+    }
+}  // namespace cppfastbox
---
+++ test/base/min_max_ct.cpp
@@ -0,0 +1,21 @@
+#include "../../include/base/min_max.h"
+#include <functional>
+using namespace cppfastbox;
+
+constexpr void test_min() noexcept
+{
+    static_assert(min(1, 2) == 1, "min(1,2) is not 1");
+    static_assert(min(1,2, std::less{}) == 1, "min(1, 2, std::less{}) is not 1");
+}
+
+constexpr void test_max() noexcept
+{
+    static_assert(max(1, 2) == 2, "max(1,2) is not 2");
+    static_assert(max(1,2, std::less{}) == 2, "max(1, 2, std::less{}) is not 2");
+}
+
+int main()
+{
+    test_min();
+    test_max();
+}
